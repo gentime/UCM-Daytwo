6ac1bca941d6a234db425669e14a4798e0000d63
gentime <36462114+gentime@users.noreply.github.com>
2018-02-27 11:26:13 -0600

    tcob 5

--- Calcutor/result.js
+++ Calcutor/result.js
@@ -15,6 +15,7 @@ function multi(){
     var num1 =document.getElementById('num1').value;
     var num2 = document.getElementById('num2').value;
     var cal = document.getElementById('cal').innerHTML = parseFloat(num1) * parseInt(num2);
+   
 
 };
 //divide 
@@ -22,12 +23,7 @@ function divi(){
     var num1 = document.getElementById('num1').value;
     var num2 = document.getElementById('num2').value;
     var cal = document.getElementById('cal').innerHTML = parseFloat(num1) / parseInt(num2);
-}
-    function alertcal(){
-alert ( parseFloat('num1')  + parseInt('num2') );
-    };
-
+    
+};
 
    
-
-
